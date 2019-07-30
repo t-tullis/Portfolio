37f1c96f86312518c0b53600ccc8546b229ec1b5
t-tullis <t.tullis34@gmail.com>
2019-07-30 01:31:26 -0700

    Updates

---
+++ precache-manifest.d0e29217bc982aa80d71c11aa4b3bae2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89844fae0325f7c8b48fb5d1ed93dc59",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "3ee29a0b5a068196e16b",
+    "url": "/Portfolio/static/css/main.4d341ff3.chunk.css"
+  },
+  {
+    "revision": "c1dcc5ac81a85a656aae",
+    "url": "/Portfolio/static/js/2.e4383a0c.chunk.js"
+  },
+  {
+    "revision": "3ee29a0b5a068196e16b",
+    "url": "/Portfolio/static/js/main.e41a0f96.chunk.js"
+  },
+  {
+    "revision": "591eb5beb99e69ed8d3e",
+    "url": "/Portfolio/static/js/runtime~main.8760695a.js"
+  },
+  {
+    "revision": "c90e3c63f9ffc6a959150fd57ce52cdb",
+    "url": "/Portfolio/static/media/Bubl.c90e3c63.gif"
+  },
+  {
+    "revision": "bb29ab5085ae7a4b9aa43b6b1f76649e",
+    "url": "/Portfolio/static/media/Calendr.bb29ab50.png"
+  },
+  {
+    "revision": "0f2bab4b9f08e8bc5f63684aaa4aa008",
+    "url": "/Portfolio/static/media/InstaClone.0f2bab4b.png"
+  },
+  {
+    "revision": "1e88e8edfd65cdf888352652ab439428",
+    "url": "/Portfolio/static/media/Resume.1e88e8ed.png"
+  }
+]);
