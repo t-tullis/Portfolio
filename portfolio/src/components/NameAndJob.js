baf4b0cf568afbac6e18e6b6d0e401d72939ead9
t-tullis <t.tullis34@gmail.com>
2019-07-31 02:05:48 -0700

    made name and job responsive and added height to particles

--- portfolio/src/components/NameAndJob.js
+++ portfolio/src/components/NameAndJob.js
@@ -4,8 +4,10 @@ import '../App.css'
 function NameAndJob(){
     return(
         <div className='nameAndJob'>
-            <h1>Terrell Tullis</h1>
-            <h3>Full-Stack Web Developer</h3>
+            <div className='nameAndJob-content'>
+                <h1>Terrell Tullis</h1>
+                <h3>Full-Stack Web Developer</h3>
+            </div>
         </div>
     )
 }
