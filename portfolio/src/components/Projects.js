29bfcda8f8c40f6ea38fcd93dc0fd067b7395047
t-tullis <t.tullis34@gmail.com>
2019-08-02 11:09:10 -0700

    added treasure-hunt project to portfolio

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -2,6 +2,7 @@ import React from 'react'
 import calendr from '../images/Calendr.png'
 import instaclone from '../images/InstaClone.png'
 import bubl from '../images/Bubl.gif'
+import treasureHunt from '../images/TreasureHunt.png'
 import '../App.css'
 
 
@@ -10,6 +11,19 @@ function Projects(){
     return(
         <div className='projects'>
             <h1>Projects</h1>
+            <div className='project-card'>
+                <img src={treasureHunt} />
+                <div className='text-content'>
+                    <h2>Treasure-Hunt</h2>
+                    <p>Treasure-Hunt is a multiplayer real-time virtual world text-based game(MUD) where you adventure through
+                        a map collecting and selling treasures.  
+                    </p>
+                    <div className='project-links'>
+                    <a href='https://treasure-hunt-legend.netlify.com/' target='_blank'>Live</a>
+                    <a href='https://github.com/treasure-hunt' target='_blank'>GitHub Repo</a>
+                    </div>
+                </div>
+            </div>
             <div className='project-card'>
                 <img src={calendr} />
                 <div className='text-content'>
