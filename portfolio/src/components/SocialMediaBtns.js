e7469d5f43281f690b0994a59d77e7b72f92a8df
t-tullis <t.tullis34@gmail.com>
2019-08-05 13:16:00 -0700

    changed name back to original

--- portfolio/src/components/SocialMediaBtns.js
+++ portfolio/src/components/SocialMediaBtns.js
@@ -5,7 +5,7 @@ function SocialMediaBtns(props){
     return(
         <div className='levelDownWhiteSpace'>
             <div className='socialMedia'>
-                <a href='https://www.github.com/terrelltullis' target='_blank'><i className="fab fa-github-alt fa-3x"></i></a>
+                <a href='https://www.github.com/t-tullis' target='_blank'><i className="fab fa-github-alt fa-3x"></i></a>
                 <a href='https://www.linkedin.com/in/terrelltullis/' target='_blank'><i className="fab fa-linkedin-in fa-3x"></i></a>
                 <i className="fab fa-twitter fa-3x"></i>
             </div>
